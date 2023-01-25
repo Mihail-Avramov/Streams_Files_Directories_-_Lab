1e32fcdf0d36f80069ef4dc4ed2dcfdac996d29d
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-25 17:19:51 +0200

    Extract Special Bytes

--- ExtractSpecialBytes/ExtractSpecialBytes.cs
+++ ExtractSpecialBytes/ExtractSpecialBytes.cs
@@ -1,7 +1,9 @@
-﻿namespace ExtractSpecialBytes
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ExtractSpecialBytes
 {
-    using System;
-    using System.IO;
     public class ExtractSpecialBytes
     {
         static void Main()
@@ -15,6 +17,29 @@
 
         public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
         {
+            using StreamReader streamReader = new StreamReader(bytesFilePath);
+
+            byte[] fileBytes = File.ReadAllBytes(binaryFilePath);
+
+            List<string> bytesList = new List<string>();
+
+            var sb = new StringBuilder();
+
+            while (!streamReader.EndOfStream)
+            {
+                bytesList.Add(streamReader.ReadLine());
+            }
+            foreach (var item in fileBytes)
+            {
+                if (bytesList.Contains(item.ToString()))
+                {
+                    sb.AppendLine(item.ToString());
+                }
+
+            }
+            using StreamWriter file = new StreamWriter(outputPath);
+            file.WriteLine(sb.ToString().Trim());
+
         }
     }
 }
