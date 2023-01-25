2977863c4d1a6e2a80c6df726af5c392cee5a41e
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-25 16:51:48 +0200

    WordCount

--- WordCount/WordCount.cs
+++ WordCount/WordCount.cs
@@ -1,9 +1,10 @@
-﻿namespace WordCount
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WordCount
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
     public class WordCount
     {
         static void Main()
@@ -17,6 +18,36 @@
 
         public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
         {
+            using StreamReader wordsReader = new StreamReader(wordsFilePath);
+            using StreamReader textReader = new StreamReader(textFilePath);
+            using StreamWriter textWriter = new StreamWriter(outputFilePath);
+
+            string[] words = wordsReader.ReadToEnd().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(t => t.ToLower()).ToArray();
+
+            Dictionary<string,int> wordCounts = new Dictionary<string,int>();
+
+            foreach (string word in words)
+            {
+                wordCounts[word] = 0;
+            }
+
+            string line = string.Empty;
+
+            while((line = textReader.ReadLine()) != null)
+            {
+                foreach (string word in words)
+                {
+                    if (line.ToLower().Contains(word))
+                    {
+                        wordCounts[word]++;
+                    }
+                }
+            }
+
+            foreach (var (word, count) in wordCounts.OrderByDescending(w => w.Value))
+            {
+                textWriter.WriteLine($"{word} - {count}");
+            }
         }
     }
 }
