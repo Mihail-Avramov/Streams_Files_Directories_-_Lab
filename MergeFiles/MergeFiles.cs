8b13ec6bc0543a5f3a16c495ca49c000a40ceda8
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-25 16:59:19 +0200

    Merge Files

--- MergeFiles/MergeFiles.cs
+++ MergeFiles/MergeFiles.cs
@@ -1,7 +1,8 @@
-﻿namespace MergeFiles
+﻿using System.IO;
+
+namespace MergeFiles
 {
-    using System;
-    using System.IO;
+
     public class MergeFiles
     {
         static void Main()
@@ -15,6 +16,30 @@
 
         public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
         {
+            using StreamReader reader1 = new StreamReader(firstInputFilePath);
+            using StreamReader reader2 = new StreamReader(secondInputFilePath);
+            using StreamWriter writer = new StreamWriter(outputFilePath);
+
+            while (true)
+            {
+                string line1 = reader1.ReadLine();
+                string line2 = reader2.ReadLine();
+
+                if (line1 == null && line2 == null)
+                {
+                    break;
+                }
+
+                if (line1 != null)
+                {
+                    writer.WriteLine(line1);
+                }
+
+                if (line2 != null)
+                {
+                    writer.WriteLine(line2);
+                }
+            }
         }
     }
 }
