6093b749f056299227339e2b1921a320a41fd85d
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-25 16:10:40 +0200

    Add project files.

---
+++ OddLines/OddLines.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFile)
+        {
+            using StreamReader reader = new StreamReader(inputFilePath);
+            using StreamWriter writer = new StreamWriter(outputFile);
+
+            int n = 0;
+            string input = string.Empty;
+
+            while ((input = reader.ReadLine()) != null)
+            {
+                if (n % 2 == 1)
+                {
+                    writer.WriteLine(input);
+                }
+
+                n++;
+            }
+
+        }
+    }
+}
